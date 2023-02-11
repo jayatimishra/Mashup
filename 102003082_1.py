f040730a5e0398bf5f6d82e96e15511fa2834979
Jayati Mishra <jmishra_be20@thapar.edu>
2023-02-12 04:19:21 +0530

    curl

--- 102003082_1.py
+++ 102003082_1.py
@@ -6,81 +6,71 @@ import moviepy.audio.fx.all as afx
 import moviepy.audio.AudioClip as ac
 from googleapiclient.discovery import build
 
-from config import api_key
 import os
+import requests
+import threading
 
 
 
+API_KEY = "AIzaSyD8R7vLoHl_4fX8b9bg523kNGiEhDD5IaY"
 
-
-
-
-def search_and_get_video_ids(query, n=5,key=api_key):
-    
-    youtube = build("youtube", "v3", key)
-
-    
-    request = youtube.search().list(part="id", type='video', q=query, maxResults=n)
-    response = request.execute()
-
-    video_ids = [item["id"]["videoId"] for item in response["items"]]
-
+print(os.environ.get('API_KEY'))
+print(os.environ)
+def search_and_get_video_ids(query, n=5,key=API_KEY):
+    url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&key={API_KEY}&type=video&q={query}&maxResults={n}'
+    response = requests.get(url).json()
+    video_ids = []
+    for item in response['items']:
+        video_ids.append(item['id']['videoId'])
     return video_ids
 
 
-
+def download_audio(yt, m, i):
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_stream.download()
+    audio_clip = AudioFileClip(audio_stream.default_filename)
+    sub_audio_clip = audio_clip.subclip(0, m)
+    sub_audio_clip.write_audiofile(f"{i}.mp3")
+    print(f"Downloaded audio from video {i+1}: {yt.title}")
 
 def download_and_concatenate_audio(video_ids, n=10, m=20):
     audio_clips = []
-
-    
-    
+    # use multi-threading to download audio of all videos simultaneously and then concatenate them
+    threads = []
     for i in range(n):
         video_id = video_ids[i]
-
-        
         yt = YouTube(f"https://www.youtube.com/watch?v={video_id}")
+        thread = threading.Thread(target=download_audio, args=(yt, m, i))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
 
-       
-        audio_stream = yt.streams.filter(only_audio=True).first()
+    for i in range(n):
+        audio_clip = AudioFileClip(f"{i}.mp3")
+        audio_clips.append(audio_clip)
+    final_audio = concatenate_audioclips(audio_clips)
+    final_audio.write_audiofile("output.mp3")
 
-        
-        audio_stream.download()
+    # remove all .mp3 files except the final output file
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".mp3") and file != "output.mp3":
+            os.remove(file)
 
-        
-        audio_clip = AudioFileClip(audio_stream.default_filename)
-
-        
-        sub_audio_clip = audio_clip.subclip(0, m)
-
-      
-        audio_clips.append(sub_audio_clip)
-
-        
-        print(f"Downloaded audio from video {i+1}: {yt.title}")
-
-    # Concatenating audio clips
-    concatenated_audio = concatenate_audioclips(audio_clips)
-
-  
-    output_path = os.path.join(os.getcwd(), "output.mp3")
-    concatenated_audio.write_audiofile(output_path)
-
-
-
+    # remove all .mp4 files
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".mp4"):
+            os.remove(file)
 
 def main(argv):
   if len(argv) != 4:
     print("Incorrect number of parameters. Please enter the correct number of parameters.")
     return
-  
   singer_name = argv[0]
   number_of_videos = int(argv[1])
   audio_duration = int(argv[2])
   output_file = argv[3]
-
-
-  video_ids = search_and_get_video_ids(singer_name, number_of_videos, api_key)
+  video_ids = search_and_get_video_ids(singer_name, number_of_videos, API_KEY)
   download_and_concatenate_audio(video_ids, number_of_videos, audio_duration)
 
 
